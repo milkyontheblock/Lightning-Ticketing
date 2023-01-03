e3eb62811e63e7c050fd27c2213d049a44607094
milkyontheblock <jordanvanwijnen0709@gmail.com>
2023-01-03 20:23:53 +0100

    Created user schema

---
+++ misc/database/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+// User schema with email and password but personal information is not required
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        minlength: 3,
+        maxlength: 100,
+        match: /^\S*$/,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        minlength: 8,
+        maxlength: 100,
+        match: /^\S*$/,
+        required: true
+    },
+    firstName: {
+        type: String,
+        minlength: 1,
+        maxlength: 100,
+        match: /^\S*$/,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        minlength: 1,
+        maxlength: 100,
+        match: /^\S*$/,
+        required: true,
+    },
+    zipCode: {
+        type: String,
+        required: false,
+        default: null
+    },
+    houseNumber: {
+        type: String,
+        required: false,
+        default: null
+    },
+});
+
+module.exports = mongoose.model('User', userSchema);
---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to MongoDB'));
+
+// Routes
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
