d2f3c18c2272e997af927a0e2bbeb6cbe38a5ccf
milkyontheblock <jordanvanwijnen0709@gmail.com>
2023-01-05 18:49:26 +0100

    Update cart.js

--- misc/database/cart.js
+++ misc/database/cart.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 // Cart schema that can hold tickets
 const cartSchema = new mongoose.Schema({
+    shop: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Shop',
+        default: null,
+    },
     tickets: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Ticket',
