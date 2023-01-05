5334d7ee6f26ea8ed684afa267a63da0b3c106e0
milkyontheblock <jordanvanwijnen0709@gmail.com>
2023-01-05 16:41:59 +0100

    Created GET /event/v1/:id

--- routes/Event/getEvent.js
+++ routes/Event/getEvent.js
@@ -1,14 +1,12 @@
 const Event = require('../../misc/database/event');
+const EntranceType = require('../../misc/database/entranceType');
 
 module.exports = async function (req, res, next) {
     try {
         // Get the event from the database 
         // and populate with entrance types
         // but remove the entrance types' event property
-        const event = await Event.findById(req.params.id).populate({
-            path: 'entranceTypes',
-            select: '-event'
-        });
+        const event = await Event.findById(req.params.id).select(['-creator', '-_id', '-createdOn', '-__v'])
 
         // Make sure the event exists
         if (!event) {
@@ -17,6 +15,12 @@ module.exports = async function (req, res, next) {
                 success: false
             })
         }
+
+        // Find the entrance types for the event
+        const entranceTypes = await EntranceType.find({event: event._id}).select('-event');
+
+        // Add the entrance types to the event
+        event.entranceTypes = [];
 
         // Return the event
         res.status(200).json({
